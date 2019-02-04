25bf55b012c2240996162b55ba601fb766f1b065
DavidPost-1 <david.posthumadeboer@physics.ox.ac.uk>
2019-02-04 22:48:26 +0000

    Add cavity bandwidth method given quality factor

--- accelerator_calculations/cavity.py
+++ accelerator_calculations/cavity.py
@@ -10,7 +10,7 @@ class cavity():
 
     def __init__(self, cavity_frequency):
         self.__frequency = cavity_frequency
-    
+
     def frequency(self):
         return self.__frequency
 
@@ -22,10 +22,14 @@ class cavity():
         a_nml = (1*pq.c/(2*np.pi) * jn_zeros(0, 1)[-1] / self.frequency()).to(pq.m)
         return a_nml
 
+    def bandwidth(self, Quality_factor):
+        bw = self.frequency() / Quality_factor
+        return bw
+
 
 def resonant_frequency(a, d, n, m, l):
     '''
-    Returns the resonant frequency 
+    Returns the resonant frequency
     Assumes that the waveguide is filled with vacuum.
     a = radius
     d = length
@@ -42,10 +46,9 @@ def resonant_frequency(a, d, n, m, l):
 
 
 if __name__ == '__main__':
-    
+
     cavity_frequency = 10 * pq.MHz
     a_nml = (1*pq.c/(2*np.pi) * jn_zeros(0, 1)[-1] / cavity_frequency).to(pq.m)
     print("a_nml = {:.2f}".format(a_nml))
-    
+
     print("TM011 mode has frequency {:.2f}".format(f_nml(a_nml, 50*pq.cm, 0, 1, 1).to(pq.MHz)))
-    
